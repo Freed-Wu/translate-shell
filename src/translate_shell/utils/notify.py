5259ef8ca8fd4038ea4483b0a4b6e3814e5855bd
minyu <minyu@outlook.com>
2023-05-24 21:57:46 +0800

    :alien: Adapt pynotifier >= 0.5.0

--- src/translate_shell/utils/notify.py
+++ src/translate_shell/utils/notify.py
@@ -2,13 +2,20 @@ r"""Notify
 ==========
 """
 import re
+from contextlib import suppress
 from shlex import split
 from shutil import which
 from subprocess import run
 
 from .. import APPNAME
 from ..__main__ import ASSETS_PATH
-from ..external.pynotifier import Notification
+
+with suppress(ImportError):
+    from pynotifier import Notification, NotificationClient
+    from pynotifier.backends import platform
+
+    client = NotificationClient()
+    client.register_backend(platform.Backend())
 
 PAT = re.compile(r"\x1b\[[0-9;]+?m")
 ICON_FILE = str(ASSETS_PATH / "images" / "translate-shell.png")
@@ -24,6 +31,7 @@ def notify(rst: str) -> None:
     :rtype: None
     """
     text = PAT.sub("", rst)
+    # https://github.com/YuriyLisovskiy/pynotifier/pull/29
     if which("termux-toast"):
         run(split("termux-toast -g top") + [text], check=True)
     if which("termux-notification"):
@@ -36,4 +44,14 @@ def notify(rst: str) -> None:
             check=True,
         )
     else:
-        Notification("Translation", text, 10, "low", ICON_FILE, APPNAME).send()
+        with suppress(NameError):
+            client.notify_all(
+                Notification(
+                    title="Translation",
+                    message=text,
+                    duration=10,
+                    urgency="low",
+                    icon_path=ICON_FILE,
+                    app_name=APPNAME,
+                )
+            )
