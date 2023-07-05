5b7f2dcb5a3f7e9619f9513dc2868f791f0d2b57
Wu Zhenyu <wuzhenyu@ustc.edu>
2023-07-05 20:00:22 +0800

    :white_check_mark: Fix pytest

--- tests/translate_test.py
+++ tests/translate_test.py
@@ -1,8 +1,5 @@
 """Test translators."""
 import json
-import sys
-
-import pytest
 
 from translate_shell.translate import translate
 
@@ -12,12 +9,9 @@ from . import ASSETS_PATH
 class Test:
     """Test."""
 
-    @pytest.mark.skipif(
-        sys.platform == "win32", reason="'charmap' codec can't decode byte"
-    )
     def test_google(self) -> None:
         """Test google"""
-        rst = vars(translate("The Mythical Man-Month", "zh_TW"))
+        rst = vars(translate("The Mythical Man-Month", "zh_CN"))
         expected = json.loads(
             (ASSETS_PATH / "json" / "google.json").read_text()
         )
