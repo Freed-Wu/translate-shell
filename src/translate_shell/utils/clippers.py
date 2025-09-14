3f477a655ea19674d1d429d5ac1f225fe48d4b29
Wu, Zhenyu <wuzhenyu@ustc.edu>
2025-09-14 13:34:37 +0800

    :sparkles: Add waypaste

--- src/translate_shell/utils/clippers.py
+++ src/translate_shell/utils/clippers.py
@@ -22,6 +22,7 @@ def get_clipper() -> list[str]:
     """
     cmds = []
     if os.getenv("WAYLAND_DISPLAY"):
+        cmds += ["waypaste -p"]
         cmds += ["wl-paste --no-newline"]
     if os.getenv("DISPLAY"):
         cmds += ["xsel -o", "xclip -o"]
